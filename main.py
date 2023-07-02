c7529a9df265082b957933ec13576ea1dca8dd27
happytoast2011 <138382264+happytoast2011@users.noreply.github.com>
2023-07-02 15:04:55 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -14,3 +14,4 @@ else:
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
     else:
         ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(__file__), None, 1)
+#123ewsddcaaf
